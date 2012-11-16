477c95a9e68279ba8df3a4efd5f9e696c0f22b0b
Ken Zhang <kenz@codeaurora.org>
2012-11-16 07:40:34 -0800

    msm: display: start timing engine after panel init
    DSI video timing engine should be kicked off after panel initialization
    
    Change-Id: Idf428f40e18432afb627a975567959c5f6da5404
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay_dsi_video.c

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -780,6 +780,7 @@ int mdp4_dsi_video_pipe_commit(int cndx, int wait);
 int mdp4_dsi_cmd_pipe_commit(int cndx, int wait);
 void mdp4_dsi_cmd_vsync_ctrl(struct fb_info *info, int enable);
 void mdp4_dsi_video_vsync_ctrl(struct fb_info *info, int enable);
+void mdp4_overlay_dsi_video_start(void);
 #ifdef CONFIG_FB_MSM_MDP303
 static inline void mdp4_dsi_cmd_del_timer(void)
 {
