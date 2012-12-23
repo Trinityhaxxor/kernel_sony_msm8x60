762ecec8906b4ee5c1cf8e612bf78892ae5125cd
RaymanFX <raymanfx@gmail.com>
2012-12-23 22:15:22 +0100

    linux: fix header files

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Dec 24 11:58:09 CET 2012"
+#define LINUX_COMPILE_BY "RaymanFX"
+#define LINUX_COMPILE_HOST "iMac21-TheHive.local"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
