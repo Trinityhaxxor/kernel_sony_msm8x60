994ea379ef5efdae70b94c4025cec3367983bd35
RaymanFX <raymanfx@gmail.com>
2013-06-02 21:41:58 +0200

    cpufreq: fix weird 2nd core behaviour
    Change-Id: I5c5a4d44db618731416f1dd08dcf05cf777d1d6d

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1063,17 +1063,25 @@ static int cpufreq_add_dev(struct device *dev, struct subsys_interface *sif)
 	/* Set governor before ->init, so that driver could check it */
 #ifdef CONFIG_HOTPLUG_CPU
 	for_each_online_cpu(sibling) {
-		struct cpufreq_policy *cp = per_cpu(cpufreq_cpu_data, sibling);
+	struct cpufreq_policy *cp = per_cpu(cpufreq_cpu_data, sibling);
 		if (cp && cp->governor &&
 		    (cpumask_test_cpu(cpu, cp->related_cpus))) {
+			printk("found sibling CPU, copying policy\n");
 			policy->governor = cp->governor;
+			policy->min = cp->min;
+			policy->max = cp->max;
+			policy->user_policy.min = cp->user_policy.min;
+			policy->user_policy.max = cp->user_policy.max;
 			found = 1;
 			break;
 		}
 	}
 #endif
 	if (!found)
+	{
+		printk("failed to find sibling CPU, falling back to defaults\n");
 		policy->governor = CPUFREQ_DEFAULT_GOVERNOR;
+	}
 	/* call driver. From then on the cpufreq must be able
 	 * to accept all calls to ->verify and ->setpolicy for this CPU
 	 */
@@ -1084,6 +1092,18 @@ static int cpufreq_add_dev(struct device *dev, struct subsys_interface *sif)
 	}
 	policy->user_policy.min = policy->min;
 	policy->user_policy.max = policy->max;
+
+	if (found)
+	{
+		/* Calling the driver can overwrite policy frequencies again */
+		struct cpufreq_policy *cp = per_cpu(cpufreq_cpu_data, sibling);
+        	printk("Overriding policy max and min with sibling settings\n");
+		policy->governor = cp->governor;
+        	policy->min = cp->min;
+        	policy->max = cp->max;
+        	policy->user_policy.min = cp->user_policy.min;
+        	policy->user_policy.max = cp->user_policy.max;
+	}
 
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 				     CPUFREQ_START, policy);
